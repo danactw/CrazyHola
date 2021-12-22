554b090bf46878c08f9425a34c21519a07399457
danachen <danac.tw@gmail.com>
2021-12-22 09:44:34 +0800

    add 1115notes-密碼加密、session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,18 @@ class UsersController < ApplicationController
   end
 
   def check
+    # STEP 1
+    # email = params[:users][:email]
+    # password = params[:users][:password]
+
+    # salted_password = "xy#{password.reverse}hellohey"
+    # encryted_password = Digest::SHA1.hexdigest(salted_password)
+
+    # u = User.find_by(email: email, password: encryted_password)
+    
+    # STEP 2 => 把step 1的東西全部放進model的一個方法裡self.login(user_info), user_info=paramss[:user]
+
+    # STEP 3 => keyword argument (請詳user.rb)
     u = User.login(params[:user])
     if u
       session[:whatever123] = u.id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 require "digest"
+# Ruby中的標準函式庫會隨ruby一起出場，但是要用他的時候要require近來才能用
 
 class User < ApplicationRecord
   validates :email, :username, presence: true
@@ -10,7 +11,11 @@ class User < ApplicationRecord
   # has_many :courses, through: :favor_courses table名稱會和第6行一樣，所以這裡要改
   has_many :favorite_courses, source: :course, through: :favor_courses
 
-  before_create :encrypt_password
+  before_create :encrypt_password # 要存檔的時候才呼叫
+  # callback、非同步
+  # library:你呼叫他寫的程式碼(function)
+  # framework:他呼叫你寫的程式碼(function)，通常有生命週期，在學框架就是在學要如何把東西放到對的地方
+
 
   def self.login(user_info)
     email = user_info[:email]
@@ -22,10 +27,25 @@ class User < ApplicationRecord
     self.find_by(email: email, password: encryted_password)
   end
 
+  # STEP 3 => keyword argument 
+  # def self.login(email: , password:)
+
+  #   salted_password = "xy#{password.reverse}hellohey"
+  #   encryted_password = Digest::SHA1.hexdigest(salted_password)
+
+  #   self.find_by(email: email, password: encryted_password)
+  # end
+
   private
   def encrypt_password
     salted_password = "xy#{self.password.reverse}hellohey"
     self.password = Digest::SHA1.hexdigest(salted_password)
+    # 說明
+    # self.password = Digest::SHA1.hexdigest(self.password)
+    # => self.password= (Digest::SHA1.hexdigest(self.password))
+    # 當我們開一個叫password的欄位時，rails model就會幫我們把每一個欄位都做一個getter＆setter
+    # self.password = Digest::SHA1.hexdigest(password) => 可執行
+    # password = Digest::SHA1.hexdigest(self.password) => 左邊的password變成一個區域變數
   end
 end
 
